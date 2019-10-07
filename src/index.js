ed5bf7543e1ec00e89c7808b9d159f5556dc09c6
VolhaP <polkhovskayaolga@mail.ru>
2019-10-07 16:54:03 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let result = 0;
+  preferences.forEach((element, index) => {
+    let first = index + 1;
+    let second = preferences[index] - 1;
+    let third =  preferences[second] - 1;
+
+    if (preferences[third] === first) {
+      result++;
+    }
+  })
+  return Math.floor(result/3);
 };
